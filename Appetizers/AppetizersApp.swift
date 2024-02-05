b60491d20b095e4ee23e95d648513e77550359ea
ivan.stoilov <ivan.stoilov@mypos.com>
2024-02-05 13:48:44 +0200

    Project creation

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ivan Stoilov on 5.02.24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
