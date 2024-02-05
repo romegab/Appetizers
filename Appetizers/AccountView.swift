d4ee5d493207f167db11346148a7b2a4f11417d6
ivan.stoilov <ivan.stoilov@mypos.com>
2024-02-05 15:16:08 +0200

    Set up tab bar structure along with the main screens

---
+++ Appetizers/AccountView.swift
@@ -0,0 +1,21 @@
+//
+//  AccountView.swift
+//  Appetizers
+//
+//  Created by Ivan Stoilov on 5.02.24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            Text("Account")
+                .navigationTitle("🤣 Account")
+        }
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ Appetizers/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  Appetizers
+//
+//  Created by Ivan Stoilov on 5.02.24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        NavigationView {
+            Text("Orders")
+                .navigationTitle("🔖 Orders")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+}
