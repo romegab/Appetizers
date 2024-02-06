d3c8ef82279d317d25e64f6899ef90ba480e035c
ivan.stoilov <ivan.stoilov@mypos.com>
2024-02-06 16:20:21 +0200

    Added network manager

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Ivan Stoilov on 6.02.24.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
