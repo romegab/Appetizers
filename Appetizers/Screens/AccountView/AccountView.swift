bc6c5d227cacc1f55abb2884969434a0e93bdb35
ivan.stoilov <ivan.stoilov@mypos.com>
2024-02-08 09:09:27 +0200

    Done with account UI

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("🤣 Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocorrectionDisabled()
+                        .autocorrectionDisabled()
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                } header: {
+                    Text("Personal info")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                        
+                }header: {
+                    Text("Requests")
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+
+            }
+            .navigationTitle("🤣 Account")
         }
     }
 }
