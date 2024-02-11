0072e80a9889d064431981deb22f27bc6fab72af
romegab <idstoilov@gmial.com>
2024-02-11 18:09:13 +0200

    Improved UX in account view

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -10,17 +10,40 @@ import SwiftUI
 struct AccountView: View {
     
     @StateObject private var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
-            Form() {
+            Form {
                 Section {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit {
+                            focusedTextField = .lastName
+                        }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit {
+                            focusedTextField = .email
+                        }
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusedTextField, equals: .email)
                         .keyboardType(.emailAddress)
                         .autocorrectionDisabled()
                         .autocorrectionDisabled()
+                        .onSubmit {
+                            focusedTextField = nil
+                        }
+                        .submitLabel(.continue)
+                    
                     DatePicker("Birthday", selection: $viewModel.user.birthdate, displayedComponents: .date)
                     
                     Button {
@@ -36,7 +59,7 @@ struct AccountView: View {
                     Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
                     Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
                         
-                }header: {
+                } header: {
                     Text("Requests")
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
@@ -46,6 +69,13 @@ struct AccountView: View {
                 viewModel.retrieveUser()
             }
             .navigationTitle("🤣 Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Dismiss") {
+                        focusedTextField = nil
+                    }
+                }
+            }
         }
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
